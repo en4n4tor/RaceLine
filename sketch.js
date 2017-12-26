2963dc0d4282c907d78c0954a52aa9892746ee4b
en4n4tor <elocnave80@gmail.com>
2017-12-25 18:48:46 -0800

    clean up
    got rid of useless comments in scetch.js

--- sketch.js
+++ sketch.js
@@ -1,25 +1,15 @@
-//var racecar;
 var pathvect1 = [];
-//var pathvect2 = [];
 var numberOfPathSegments = 72;
 var pathvect1FirstPos = new p5.Vector(-100, 0);
-//var pathvect2FirstPos = new p5.Vector(400, 200);
-
 
 function setup() {
     
 	createCanvas (800, 600);
 	
 	background(0);
-	//fill out pathvect1[] 
 	for (var i = 0; i < numberOfPathSegments; i++) {
 		pathvect1[i] = new Pathvect(5 + numberOfPathSegments - i, pathvect1FirstPos);
 	}
-	//fill out pathvect2[] 
-	//for (var i = 0; i < numberOfPathSegments; i++) {
-	//	pathvect2[i] = new Pathvect(5 + numberOfPathSegments - i, pathvect2FirstPos);
-	//}
-		
 }
 
 function draw() {
@@ -38,24 +28,13 @@ function draw() {
 		}
 	}
 	
-	//for (var i = 0; i < pathvect2.length; i++){
-	//	if (i === 0){
-	//	pathvect2[i].render(200,-100, 0, 0, 0);	
-	//	}
-	//	else {
-	//		pathvect2[i].render(pathvect2[i-1].posision.x, pathvect2[i-1].posision.y, 0, i * 10, 0);
-	//	}
-	//}
-	
 	if (mouseIsPressed){
-		//pathvect.update();
 		for (var i = 0; i < pathvect1.length; i++){
 			pathvect1[i].update();
 		}	
 	}
 	
 	if (keyIsPressed){
-		//pathvect.update();
 		for (var i = 0; i < pathvects.length; i++){
 			pathvects[i].resetVect();
 		}	
