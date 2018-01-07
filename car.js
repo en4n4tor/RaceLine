b645e3bf377541ddb901a999c329c892e8fa6445
en4n4tor <elocnave80@gmail.com>
2018-01-07 14:08:49 -0900

    more clean up
    just look at the history

--- car.js
+++ car.js
@@ -1,13 +1,15 @@
 function RaceCar() {
     
 	this.pos = createVector(width /2, height / 2);
-    this.r = 10;
+    this.r = 20;
     this.heading = PI;
     
     this.render = function() {
-        translate(this.pos.x, this.pos.y);
-        rotate(this.heading);
-        triangle(-this.r, this.r, this.r, this.r, 0, -this.r);    
+        //translate(this.pos.x, this.pos.y);
+        //rotate(this.heading);
+        stroke(200, 200, 0);
+		fill(0);
+		triangle(-this.r, this.r, this.r, this.r, 0, -this.r);    
     
     }        
    this.turn = function(angle){
--- pathvect.js
+++ pathvect.js
@@ -2,22 +2,21 @@ function Pathvect(mass, oldpos) {
 	this.mousevect 		= new p5.Vector(mouseX, mouseY);
 	this.oldpos 		= new p5.Vector(oldpos.x, oldpos.y);
 	this.vectHeading 	= new p5.Vector(0, 0);
-	this.posision		= new p5.Vector(0, 0);
+	this.position		= new p5.Vector(0, 0);
 	this.velocity 		= new p5.Vector(0, 0);
 	this.controlPoint1 	= new p5.Vector(0, 0);
     this.controlPoint2 	= new p5.Vector(0, 0);
     this.acceleration 	= new p5.Vector(0, 0);
 	this.newpos 		= new p5.Vector(0, 0);
-	//this.oldpos.set(oldpos);
 	this.mass = mass;
 	this.maxdist = 10;
 	
 
-//updates the vector posisions    
+//updates the vector positions    
 this.update = function(){
 	this.vectHeading.set(this.velocity);
 	this.vectHeading.normalize();
-	this.newpos.set(this.posision.x, this.posision.y);
+	this.newpos.set(this.position.x, this.position.y);
 	this.mousevect.set(mouseX - width/2, mouseY - height/2);
 	this.mousevect.sub(this.newpos);
 	this.applyForce(this.mousevect);
@@ -25,7 +24,7 @@ this.update = function(){
 	this.velocity.mult(.1);
 	print(this.velocity.heading())
 	
-	this.posision.add(this.velocity);
+	this.position.add(this.velocity);
 	this.acceleration.mult(0);
 		
 }
@@ -35,19 +34,19 @@ this.applyForce = function(force){
 	this.f.div(this.mass);
 	this.acceleration.add(this.f);
 }
-//draws a representation of posision vector on canvas
+//draws a representation of position vector on canvas
 this.render = function(x, y, r, g, b) {
     this.oldpos.set(x, y);
     this.controlPoint1.set(this.vectHeading.x, this.vectHeading.y);
-    this.controlPoint1.mult(this.oldpos.dist(this.posision));
+    this.controlPoint1.mult(this.oldpos.dist(this.position));
     
-    this.controlPoint2.set(this.posision.x, this.posision.y);
+    this.controlPoint2.set(this.position.x, this.position.y);
     this.controlPoint2.sub(this.controlPoint1);
     this.controlPoint1.sub(this.oldpos.copy());
 	
     stroke(100, g, b);
     line(	
-				this.posision.x, 		this.posision.y, 
+				this.position.x, 		this.position.y, 
 		 		this.controlPoint2.x, 	this.controlPoint2.y
 		);
     
@@ -62,7 +61,7 @@ this.render = function(x, y, r, g, b) {
 				this.oldpos.x, 			this.oldpos.y,
 				this.controlPoint1.x, 	this.controlPoint1.y, 
 		   		this.controlPoint2.x, 	this.controlPoint2.y,
-				this.posision.x, 		this.posision.y
+				this.position.x, 		this.position.y
 		  );
     
 			
@@ -73,7 +72,7 @@ this.render = function(x, y, r, g, b) {
 //resets vectors, called by whenKeyIsPressed in sketch 
 this.resetVect = function(){
 	this.acceleration.mult(0);
-	this.posision.mult(0);
+	this.position.mult(0);
 	this.velocity.mult(0);
 }
 }
