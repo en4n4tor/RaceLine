902a5249683730538cbd79100c3ddfee60c23282
en4n4tor <elocnave80@gmail.com>
2018-01-07 19:03:49 -0900

    got it to workish
    killed a line in pathvect that was calling a bad var

--- pathvect.js
+++ pathvect.js
@@ -5,17 +5,22 @@ function pathVect(init_p0, init_p1, init_p2, init_p3){
 	this.p2 = new p5.Vector(init_p2.x,init_p2.y);
 	this.p3 = new p5.Vector(init_p3.x,init_p3.y);
 	
-	this.update = function () {
-		this.p3.set(mouseX-width/2, mouseY-height/2);
+	this.update = function (mousepress) {
 		
-		this.p1.set(carVel.x, carVel.y);
+		this.p3.set(mouseX-width/2, mouseY-height/2);
+		//this.p1.set(carVel.x, carVel.y);
 	};
 	
 	this.show = function () {
 		
 		noFill();
 		stroke('blue');
-		bezier(this.p0.x, this.p0.y, this.p1.x, this.p1.y, this.p2.x, this.p2.y, this.p3.x, this.p3.y);
+		bezier	(
+				this.p0.x, 		this.p0.y, 
+				this.p1.x, 		this.p1.y, 
+				this.p2.x, 		this.p2.y, 
+				this.p3.x, 		this.p3.y
+				);
 		fill(255);
 		steps = 10;
 				
